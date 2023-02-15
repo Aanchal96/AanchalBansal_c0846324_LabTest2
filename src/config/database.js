2622c7016bf63d7818a6b946b829768b7d496d59
Aanchal <bansalaanchal96@gmail.com>
2023-02-15 15:46:47 -0500

    AWS Database connected

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,19 @@ const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
     host: 'labtest2.cv7cozifsfda.us-east-2.rds.amazonaws.com',
-    user: 'aanchal',
+    user: 'Aanchal',
     database: 'labtest2',
     password: 'qwerty123',
 });
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+  
+    console.log('Connected to database.');
+  });
+  
 
 module.exports = connection;
